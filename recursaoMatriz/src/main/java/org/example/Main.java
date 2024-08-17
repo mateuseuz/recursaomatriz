2312a61f47c84ab3b0d35251f9c07c95e6dfdd79
Mateus Euzébio <102334919+mateuseuz@users.noreply.github.com>
2024-08-16 23:35:06 -0300

    Add files via upload

--- recursaoMatriz/src/main/java/org/example/Main.java
+++ recursaoMatriz/src/main/java/org/example/Main.java
@@ -3,17 +3,30 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         int[][] matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        // (i, j) -> (linha, coluna)
         int i = 0, j = 0;
 
-        exibirRecursivamente(matriz, i, j) {
-            // Caso base: se [i] superar a quantidade de linhas e [j] superar a quantidade de colunas
-            if(i > matriz.length && j > matriz[matriz.length].length) {
-                return;
-            }
+        exibirRecursivamente(matriz, i, j);
+    }
 
+    public static void exibirRecursivamente(int[][] matriz, int i, int j) {
+        // Caso base: quando todas as linhas forem percorridas
+        if (i >= matriz.length) {
+            return;
+        }
 
+        // Exibe o elemento atual
+        System.out.print(matriz[i][j] + " ");
 
-            exibirRecursivamente(matriz, i, j+1);
+        // Verifica se ainda não passou pelo último elemento da linha
+        if(j < matriz[i].length - 1) {
+            // Continua na mesma linha, avançando para a coluna seguinte
+            exibirRecursivamente(matriz, i, j + 1);
+        } else {
+            // Avança para a linha seguinte e reinicia a coluna
+            System.out.println();
+            exibirRecursivamente(matriz, i + 1, 0);
         }
     }
 }
